c5becc8e2033cfb2d567b0904f9245989e3e1f1b
AlexCarolan <AlexCarolan0@gmail.com>
2021-03-22 11:55:32 +0000

    Setup ticket entity

--- source/src/main/java/com/example/helpqueue/restservice/resources/Ticket.java
+++ source/src/main/java/com/example/helpqueue/restservice/resources/Ticket.java
@@ -1,4 +1,68 @@
 package com.example.helpqueue.restservice.resources;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "Tickets")
 public class Ticket {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotBlank
+    private String name;
+    @ManyToOne
+    @JoinColumn(name="creator_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User creator;
+    @NotBlank
+    private String description;
+    @Basic
+    @NotBlank
+    private java.sql.Timestamp created;
+
+    public Ticket() {
+        super();
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
 }
--- source/src/main/java/com/example/helpqueue/restservice/resources/User.java
+++ source/src/main/java/com/example/helpqueue/restservice/resources/User.java
@@ -2,6 +2,8 @@ package com.example.helpqueue.restservice.resources;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "Users")
@@ -9,11 +11,14 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private long id;
     @NotBlank
     private boolean admin;
     @NotBlank
     private String name;
+    @OneToMany(targetEntity=Ticket.class, mappedBy="creator",cascade=CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Ticket> user = new ArrayList<>();
 
     public User() {
         super();
