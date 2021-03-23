270d65355873bd3bcfc15fa71524c8ae61945268
AlexCarolan <AlexCarolan0@gmail.com>
2021-03-23 17:31:57 +0000

    Updated test validation method

--- source/src/test/java/com/example/helpqueue/restservice/controllers/UserControllerTest.java
+++ source/src/test/java/com/example/helpqueue/restservice/controllers/UserControllerTest.java
@@ -2,19 +2,17 @@ package com.example.helpqueue.restservice.controllers;
 
 import com.example.helpqueue.restservice.resources.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ComponentScan(basePackages = {"com.example.helpqueue.*"})
 @SpringBootTest
@@ -31,18 +29,28 @@ class UserControllerTest {
     @Test
     void getById() throws Exception {
         RequestBuilder request = MockMvcRequestBuilders.get("/users/1");
-        MvcResult result = mockMvc.perform(request).andReturn();
-
-        String json = result.getResponse().getContentAsString();
-        User user = new ObjectMapper().readValue(json, User.class);
-
-        assertEquals(user.getId(), 1);
-        assertEquals(user.getName(), "Bill");
-        assertTrue(user.isAdmin());
+        mockMvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", Matchers.is(1)))
+                .andExpect(jsonPath("$.name", Matchers.is("Bill")))
+                .andExpect(jsonPath("$.admin", Matchers.is(true)));
     }
 
     @Test
-    void getAll() {
+    void getAll() throws Exception {
+//        RequestBuilder request = MockMvcRequestBuilders.get("/users/");
+//        MvcResult result = mockMvc.perform(request)
+//                .andExpect(status().isOk())
+//                .andReturn();
+//
+//        String json = result.getResponse().getContentAsString();
+//        User[] user = new ObjectMapper().readValue(json, User[].class);
+//
+//        System.out.println(json + "\n\n\n here \n\n\n");
+//
+//        assertEquals(user[0].getId(), 1);
+//        assertEquals(user[0].getName(), "Bill");
+//        assertTrue(user[0].isAdmin());
     }
 
     @Test
--- source/src/test/resources/data.sql
+++ source/src/test/resources/data.sql
@@ -1,1 +1,2 @@
 INSERT INTO Users VALUES (1 , true, 'Bill');
+INSERT INTO Users VALUES (2 , false, 'Alex');
