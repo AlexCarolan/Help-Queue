b21aac94959305ec8882cc67d0f5a0557371f313
AlexCarolan <AlexCarolan0@gmail.com>
2021-03-23 14:09:20 +0000

    Setup h2 test database

---
+++ source/src/test/java/com/example/helpqueue/restservice/services/UserServiceTest.java
@@ -0,0 +1,39 @@
+package com.example.helpqueue.restservice.services;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
